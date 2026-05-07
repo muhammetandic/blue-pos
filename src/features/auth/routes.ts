23f9a1f36f654ab2786942c2249206ffbc1197ac
Muhammet Andic <mandic@192.168.1.12>
2026-05-07 23:53:15 +0300

    added try catch to mail sending

--- src/features/auth/routes.ts
+++ src/features/auth/routes.ts
@@ -1,19 +1,7 @@
 import { Hono } from 'hono';
 import { validator } from '../../utils/validator';
-import {
-  confirmUser,
-  login,
-  register,
-  resetPassword,
-  sendOTP,
-  verifyOTP,
-} from './controller';
-import {
-  mailAuthSchema,
-  otpSendSchema,
-  otpVerifySchema,
-  resetPasswordSchema,
-} from './validators';
+import { confirmUser, login, register, resetPassword, sendOTP, verifyOTP } from './controller';
+import { mailAuthSchema, otpSendSchema, otpVerifySchema, resetPasswordSchema } from './validators';
 
 const authRoutes = new Hono();
 
@@ -23,10 +11,6 @@ authRoutes.post('/confirm', validator('json', otpVerifySchema), confirmUser);
 authRoutes.post('/forgot-password', validator('json', otpSendSchema), sendOTP);
 authRoutes.post('/verify-otp', validator('json', otpVerifySchema), verifyOTP);
 authRoutes.post('/resend-otp', validator('json', otpSendSchema), sendOTP);
-authRoutes.post(
-  '/reset-password',
-  validator('json', resetPasswordSchema),
-  resetPassword,
-);
+authRoutes.post('/reset-password', validator('json', resetPasswordSchema), resetPassword);
 
 export default authRoutes;
--- src/workers/mail.ts
+++ src/workers/mail.ts
@@ -2,9 +2,13 @@ import { Resend } from 'resend';
 
 declare var self: Worker;
 const resend = new Resend(process.env.RESEND_API_KEY);
-const from = process.env.MAIL_FROM || 'AntPOS <noreply@antpos.com.tr>';
+const from = process.env.MAIL_FROM || 'AntPOS <noreply@antpos.info>';
 
 self.onmessage = async (event: MessageEvent) => {
   const { to, subject, html } = event.data;
-  await resend.emails.send({ from, to, subject, html });
+  try {
+    await resend.emails.send({ from, to, subject, html });
+  } catch (error) {
+    console.error(error);
+  }
 };
